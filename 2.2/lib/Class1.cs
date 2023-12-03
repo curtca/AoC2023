37d7b2f0da4febd0b02af3fc20df715346842074
curtca <curt.carpenter@live.com>
2023-12-02 19:52:26 -0800

    2.2

---
+++ 2.2/lib/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace lib;
+
+public class Cubes
+{
+    public static long SumOfPowers(string input)
+    {
+        return input.Split(Environment.NewLine).Sum(line => {
+            string[] parts = line.Split(": ");
+            string[] rounds = parts[1].Split("; ");
+            long gamenum = long.Parse(parts[0].Split(' ')[1]);
+            long red = 0, green = 0, blue = 0;
+            foreach (var round in rounds)
+            {
+                string[] draws = round.Split(", ");
+                foreach (var draw in draws)
+                {
+                    string[] drawParts = draw.Split(' ');
+                    int count = int.Parse(drawParts[0]);
+                    string color = drawParts[1];
+
+                    if (color == "red" && count > red)
+                        red = count;
+                    if (color == "green" && count > green)
+                        green = count;
+                    if (color == "blue" && count > blue)
+                        blue = count;
+                }
+            }
+            return red * green * blue;
+        });
+    }
+}
