a5f800b750adad3a083d2c970e98b17ff0b933a3
curtca <curt.carpenter@live.com>
2023-12-02 17:45:32 -0800

    1.1 simplified

--- 1.1/lib/Class1.cs
+++ 1.1/lib/Class1.cs
@@ -9,9 +9,6 @@ public class Calibration
     public static long CalibrationSums(string input)
     {
         return (long) input.Split(Environment.NewLine)
-            .Sum(s => { return 
-                10 * char.GetNumericValue(s.First(c => char.IsDigit(c)))
-                + char.GetNumericValue(s.Last(c => char.IsDigit(c))); }
-        );
+            .Sum(s => long.Parse("" + s.First(c => char.IsDigit(c)) + s.Last(c => char.IsDigit(c))));
     }
 }
