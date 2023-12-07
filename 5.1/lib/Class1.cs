063a69531f470fa2f7dae22eb498d9bd456a82fc
Curt Carpenter <curt.carpenter@live.com>
2023-12-06 16:28:07 -0800

    5.1

--- 5.1/lib/Class1.cs
+++ 5.1/lib/Class1.cs
@@ -6,11 +6,30 @@ using System.Net.Http.Headers;
 
 namespace lib;
 
-public class Class
+public class Seeds
 {
-    public static long Function(string input) {
-        string[] lines = input.Split(Environment.NewLine);
-        return 0;
+    public static long LowestLocation(string input) {
+        string[] sections = input.Split("" + Environment.NewLine + Environment.NewLine);
+        var seeds = sections[0].Split(": ")[1].Split(' ').Select(strnum => long.Parse(strnum));
+        var maps = sections.Skip(1).Select(section => 
+            section.Split(Environment.NewLine).Skip(1).Select(line =>
+                line.Split(' ').Select(strnum => long.Parse(strnum)).ToArray()
+        ));
+
+        var distances = seeds.Select(seed => {
+            foreach (var map in maps) {
+                foreach (var submap in map) {
+                    if (seed >= submap[1] && seed < submap[1] + submap[2]) {
+                        seed += submap[0] - submap[1];
+                        break;
+                    }
+                }
+            }
+
+            return seed;
+        });
+
+        return distances.Min();
     }
 
 }
