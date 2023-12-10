516ba9754ce404fc8d3b3204f23aea48bda23d83
curtca <curt.carpenter@live.com>
2023-12-10 08:38:54 -0800

    8.1

--- 8.1/lib/Class1.cs
+++ 8.1/lib/Class1.cs
@@ -6,11 +6,26 @@ using System.Net.Http.Headers;
 
 namespace lib;
 
-public class Class
+public class Wasteland
 {
-    public static long Function(string input) {
+    public static long Steps(string input) {
         string[] lines = input.Split(Environment.NewLine);
-        return 0;
+        var turns = lines[0].ToArray();
+        var map = new Dictionary<string, (string, string)>();
+        foreach (var line in lines.Skip(2)) {
+            // AAA = (BBB, CCC)
+            map.Add(line.Substring(0, 3), (line.Substring(7, 3), line.Substring(12, 3)));
+        }
+        string here = "AAA";
+        long steps = 0, iturns = 0;
+
+        while (here != "ZZZ") {
+            here = turns[iturns] == 'L' ? map[here].Item1 : map[here].Item2;
+            steps++;
+            iturns = (iturns + 1) % turns.Length;
+        }
+
+        return steps;
     }
 
 }
