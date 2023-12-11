f133e6bbbf0e46fd018c67cfc47d19a74e69a5e2
curtca <curt.carpenter@live.com>
2023-12-10 19:27:54 -0800

    9.1

---
+++ 9.1/lib/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace lib;
+
+public class Oasis
+{
+    public static long SumOfExtrapolated(string input) {
+        return input.Split(Environment.NewLine).Sum(line => {
+            var nums = line.Split(' ').Select(n => long.Parse(n)).ToList();
+            long sum = 0;
+            while (!nums.All(n => n == 0)) {
+                sum += nums.Last();
+                for (int i = 1; i < nums.Count; i++) {
+                    nums[i - 1] = nums[i] - nums[i-1];
+                }
+                nums.RemoveAt(nums.Count - 1);
+            }
+            return sum;
+        });
+    }
+
+}
