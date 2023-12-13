4e01b1de5739b2e8c48d4c472caba3aefc3ec071
curtca <curt.carpenter@live.com>
2023-12-13 08:07:06 -0800

    12.2 failed attempt (need to debug and memoize)

--- 12.2/lib/Class1.cs
+++ 12.2/lib/Class1.cs
@@ -4,8 +4,16 @@ using System.Dynamic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Security.Cryptography;
+using System.Diagnostics;
 
 namespace lib;
+
+/*
+New plan:
+Recursive search where each call is to place 1 piece. 
+Body then recursively cals for all locations of next piece
+
+*/
 
 public class HotSprings
 {
@@ -18,24 +26,44 @@ public class HotSprings
             template = parts[0] + '?' + parts[0] + '?' + parts[0] + '?' + parts[0] + '?' + parts[0];
             sizes = parts[1].Split(',').Select(i => int.Parse(i)).ToArray();
             sizes = sizes.Concat(sizes).Concat(sizes).Concat(sizes).Concat(sizes).ToArray();
-            return Permutations("");
+            long perms = Permutations(0, 0);
+            Debug.WriteLine(perms);
+            return perms;
         });
     }
 
-    private static long Permutations(string sofar)
+    private static long Permutations(int pos, int piece)
     {
-        // Lame brute force approach: Generate every possible combo ./# of correct length and validate
-        if (sofar.Length == template.Length)
-            return IsValid(sofar) ? 1 : 0;
-        else {
-            long sum = 0;
-            if (template[sofar.Length] != '#')
-                sum += Permutations(sofar + ".");
-            if (template[sofar.Length] != '.')
-                sum += Permutations(sofar + "#");
-            return sum;
+        // Build only valid permutations rather than checking them after generated
+        // Try putting this piece here AND at subsequent spaces to the right (padded by spaces)
+        // Can we even place this piece here? 
 
+        int maxspaces = template.Length - pos; // how many spaces can we possibly shift next piece
+        for (int i = piece; i < sizes.Length; i++)
+            maxspaces -= sizes[i];
+        maxspaces -= sizes.Length - piece - 1;
+
+        long sum = 0;
+
+        for (int space = 0; space <= maxspaces; space++) {
+            bool canPlaceHere = true;
+            for (int i = 0; i < sizes[piece]; i++) {
+                if (template[pos + i] == '.') {
+                    canPlaceHere = false;
+                    break;
+                }
+            }
+            bool lastPiece = piece == sizes.Length - 1;
+
+            if (canPlaceHere && (lastPiece || (template[pos + sizes[piece] + space] != '#'))) { // need a space after each piece
+                if (lastPiece)
+                    sum ++;
+                else
+                    sum += Permutations(pos + sizes[piece] + space + 1, piece + 1);
+            }
         }
+
+        return sum;
     }
 
     private static bool IsValid(string sofar)
